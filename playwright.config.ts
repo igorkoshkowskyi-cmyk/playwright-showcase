6f664ac55e2b7e083e163167b10a160e7538632d
Igor <igor.koshkowskyi@gmail.com>
2026-06-09 22:57:31 +0300

    update allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,32 +22,41 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ["html"],
+    ["allure-playwright",
+      {
+        detail: true,
+        outputFolder: "allure-results",
+        suiteTitle: false,
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://www.saucedemo.com',
+    baseURL: "https://www.saucedemo.com",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     // Setup project — запускается первым
-    { 
-        name: 'setup', 
-        testMatch: /.*\.setup\.ts/  // только файлы *.setup.ts
+    {
+      name: "setup",
+      testMatch: /.*\.setup\.ts/, // только файлы *.setup.ts
     },
-    
+
     // Главный test project — зависит от setup
     {
-        name: 'chromium',
-        use: { 
-            ...devices['Desktop Chrome'],
-            storageState: 'playwright/.auth/user.json',  // ← загружаем auth state
-        },
-        dependencies: ['setup'],  // ← запустить setup перед мной
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: "playwright/.auth/user.json", // ← загружаем auth state
+      },
+      dependencies: ["setup"], // ← запустить setup перед мной
     },
 
     /* Test against mobile viewports. */
