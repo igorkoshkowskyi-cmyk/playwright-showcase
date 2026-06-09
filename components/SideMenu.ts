2d3dbe92726ccb3f3f1a2f99b38534da1495ba37
Igor <igor.koshkowskyi@gmail.com>
2026-06-09 21:32:26 +0300

    1 step githubActions

--- components/SideMenu.ts
+++ components/SideMenu.ts
@@ -1,25 +1,25 @@
-import {Page, Locator} from '@playwright/test';
+import { Locator, Page } from "@playwright/test";
 
 export class SideMenu {
-    readonly page: Page;
+  readonly page: Page;
 
-    private readonly openMenuBtn: Locator;
-    private readonly logoutBtn: Locator;
+  private readonly openMenuBtn: Locator;
+  private readonly logoutBtn: Locator;
 
-    constructor (page:Page) {
-        this.page = page;
+  constructor(page: Page) {
+    this.page = page;
 
-        this.openMenuBtn = page.getByRole('button', {name: 'Open Menu'});
-        this.logoutBtn = page.getByRole('link', {name: 'Logout'});
-    }
+    this.openMenuBtn = page.getByRole("button", { name: "Open Menu" });
+    this.logoutBtn = page.getByRole("link", { name: "Logout" });
+  }
 
-    // open menu action
-    async openMenu() {
-        await this.openMenuBtn.click();
-    };
+  // open menu action
+  async openMenu() {
+    await this.openMenuBtn.click();
+  }
 
-    // logout action
-    async logoutAction() {
-        await this.logoutBtn.click();
-    };
-};
+  // logout action
+  async logoutAction() {
+    await this.logoutBtn.click();
+  }
+}
